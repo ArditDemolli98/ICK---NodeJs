f41214f0d3123a68cdb81d6063c4c7fa922477f2
ArditDemolli98 <n_arditi_98@hotmail.com>
2023-11-12 21:19:53 +0100

    Session 5 server modified

--- session_5/server.js
+++ session_5/server.js
@@ -2,19 +2,41 @@ const http = require("http");
 
 const server = http.createServer((req, res)=>{
     const url = req.url;
-    if(url === "/arditi"){
+    
+    if(url === "/about"){
         res.writeHead(200, {"Content-Type" : "text/html"})
         res.write("<html>");
-        res.write("<head><title>Serveri</title></head>")
-        res.write("<body><h1>Hello from Arditi</h1></body>")
+        res.write("<head><title>About</title></head>")
+        res.write("<body><h1>This is the about page</h1></body>")
+        res.write("</html>");
+        return res.end();
+    } else if (url === "/career") {
+        res.writeHead(200, {"Content-Type" : "text/html"})
+        res.write("<html>");
+        res.write("<head><title>Career</title></head>")
+        res.write("<body><h1>This is the career page</h1></body>")
+        res.write("</html>");
+        return res.end();
+    } else if (url === "/contact") {
+        res.writeHead(200, {"Content-Type" : "text/html"})
+        res.write("<html>");
+        res.write("<head><title>Contact</title></head>")
+        res.write("<body><h1>This is the contact page</h1></body>")
+        res.write("</html>");
+        return res.end();
+    } else if(url !== "/" ) {
+        res.writeHead(400, {"Content-Type" : "text/html"})
+        res.write("<html>");
+        res.write("<head><title>Error</title></head>")
+        res.write("<body><h1>Page is missing</h1></body>")
         res.write("</html>");
         return res.end();
     }
     res.writeHead(200, {"Content-Type" : "text/html"})
     res.write("<html>");
-    res.write("<head><title>Serveri</title></head>")
-    res.write("<body><h1>Hello from NodeJs</h1></body>")
+    res.write("<head><title>Home</title></head>")
+    res.write("<body><h1>This is the home page</h1></body>")
     res.write("</html>");
     return res.end();
 })
-server.listen(8000)
+server.listen(8000);
