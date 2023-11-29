dd928cdc967f13465cc54632a34906b5c0ce9709
ArditDemolli98 <n_arditi_98@hotmail.com>
2023-11-29 11:05:20 +0100

    User middleware created

--- session_7/express-app/app.js
+++ session_7/express-app/app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
+const bodyParser = require("body-parser");
 
 const adminRoutes = require("./routes/adminRoutes");
 const mainRoutes = require("./routes/mainRoutes");
-
-const path = require("path");
-const bodyParser = require("body-parser");
+const User = require("./models/User");
 
 const PORT = 8000;
 
@@ -19,14 +19,33 @@ mongoose.connect("mongodb+srv://arditdemolli98:ardos@cluster0.lpzt4xk.mongodb.ne
         }else{
             console.log(`Server is running on port ${PORT}`);
         }
-    });
+    }); 
 })
 .catch(err => console.log(err));
 
 app.use(express.static(path.join(__dirname, "public")))
 app.use(bodyParser.urlencoded({extended: false}))
-
 app.set("view engine", "ejs");
+
+app.use((req, res, next) => {
+    User.findOne()
+    .then(user =>{
+        if(!user){
+            const user = new User({
+                username: "ardit",
+                email: "ardos@ardos.com",
+                password: "ardos"
+            })
+            user.save()
+            .then(result => {
+                req.user = user;
+                next();
+            })
+        }
+        req.user = user;
+        next();
+    })
+})
 
 app.use("/", mainRoutes)
 
