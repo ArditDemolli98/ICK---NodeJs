11d415eeda0a0fe34074532d3ef0bf13dd365807
ArditDemolli98 <n_arditi_98@hotmail.com>
2023-12-06 10:11:49 +0100

    .env package added

--- session_7/express-app/app.js
+++ session_7/express-app/app.js
@@ -6,28 +6,25 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const MongoDBStore = require('connect-mongodb-session')(session);
 const flash = require("connect-flash");
+require("dotenv").config({path: "./config/.env"});
 
 const adminRoutes = require("./routes/adminRoutes");
 const mainRoutes = require("./routes/mainRoutes");
 const authRoutes = require("./routes/authRoutes");
 
-
-const PORT = 8000;
-const DB_URI = "mongodb+srv://arditdemolli98:ardos@cluster0.lpzt4xk.mongodb.net/";
-
 const store = new MongoDBStore({
-    uri: DB_URI,
+    uri: process.env.DB_URI,
     collection: "sessions"
 })
 
-mongoose.connect(DB_URI)
+mongoose.connect(process.env.DB_URI)
 .then(result =>{
     console.log("Connected to the database");
-    app.listen(PORT, (error) =>{
+    app.listen(process.env.PORT, (error) =>{
         if(error) {
             console.log(error);
         }else{
-            console.log(`Server is running on port ${PORT}`);
+            console.log(`Server is running on port ${process.env.PORT}`);
         }
     }); 
 })
