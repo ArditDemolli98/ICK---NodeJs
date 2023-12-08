fd01261dad47556dc124cc4ede5cceff20eb35ef
ArditDemolli98 <n_arditi_98@hotmail.com>
2023-12-08 10:25:47 +0100

    GetById and Delete methods - Added

--- session_17/rest_api/controllers/feedController.js
+++ session_17/rest_api/controllers/feedController.js
@@ -32,6 +32,25 @@ module.exports = {
         }) 
     },
 
+    getPostById: (req, res) =>{
+        const postId = req.params.id;
+
+        Post.findById(postId)
+        .then(post => {
+            if(!post) {
+                res.status(404).json({
+                    message: "This post could not be found!"
+                })
+            }
+            res.status(200).json({
+                post
+            })   
+        })
+        .catch(err=>{
+            res.status(500).json({message: err.message});
+        })
+    },
+
     updatePost: (req, res) =>{
         const postId = req.params.id;
         const title = req.body.title;
@@ -52,6 +71,24 @@ module.exports = {
                     message: "Post updated successfully",
                     post: result
                 })
+            })
+        })
+        .catch(err=>{
+            res.status(500).json({message: err.message});
+        })
+    },
+
+    deletePost: (req, res) => {
+        const postId = req.params.id;
+
+        Post.findById(postId)
+        .then(post => {
+            if(!post){
+                res.status(404).json({message: "Post could not be found!"});
+            }
+            return Post.findOneAndDelete({_id: postId})
+            .then(result => {
+                res.status(200).json({message: "Post deleted successfully"});
             })
         })
         .catch(err=>{
--- session_17/rest_api/routes/feedRoutes.js
+++ session_17/rest_api/routes/feedRoutes.js
@@ -6,13 +6,16 @@ const feedController = require("../controllers/feedController");
 // GET/feed/posts
 router.get("/posts", feedController.getPosts);
 
+// GET/feed/post/:id
+router.get("/post/:id", feedController.getPostById);
+
 // POST/feed/createPost
 router.post("/createPost", feedController.createPost);
 
-
+// PUT/feed/updatePost
 router.put("/updatePost/:id", feedController.updatePost)
 
 
-// router.delete()
+router.delete("/deletePost/:id", feedController.deletePost);
 
 module.exports = router;
